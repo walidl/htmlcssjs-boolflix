c7efdc28bae114d8b60e29e162e008c0cf3beaa2
walidl <46167347+walidl@users.noreply.github.com>
2019-03-26 10:23:09 +0100

    milestone 3 in progress

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function printFilms(data,voto){
   var film = comp(data);
   var item = $(film);
 
-  var stars = item.children("li.stars").children(".fa-star");
+  // riempimento stelle
+  
+  var stars = item.find("li.stars").children(".fa-star");
 
   for (var i = 0; i < voto; i++) {
     stars.eq(i).addClass("accesa");
@@ -37,22 +39,51 @@ function getFlag(lang){
   }
 }
 
-function getResults(list){
+// la funzione mi prepara i dati da passare a handlebars nei vari casi per
+// tv/movie - poster - bandiera/codice-lingua
+
+function getResults(list,type){
 
   for (var i = 0; i < list.length; i++) {
 
     var item = list[i];
     var voto = Math.ceil(item.vote_average/2);
-
     var dati = {
 
-      titolo: item.title,
-      ogTitolo: item.original_title,
-      rating: voto
-      // lingua: item.original_language,
+      rating: voto,
     }
 
-    var flagUrl = getFlag(item.original_language)
+    //gestione poster
+
+    if (item.poster_path != null){
+
+      dati.poster = "https://image.tmdb.org/t/p/w154/" + item.poster_path;
+    }
+    else{
+
+      dati.poster = "img/no-image.jpg"
+    }
+
+
+    //caso movie
+
+    if(type == "movie"){
+
+      dati.titolo = item.title;
+      dati.ogTitolo = item.original_title;
+      dati.tipo = "film"
+    }
+    // caso tv
+    else if(type == "tv"){
+
+      dati.titolo = item.name;
+      dati.ogTitolo = item.original_name;
+      dati.tipo = "tv-show"
+    }
+
+    // gestione bandiere
+
+    var flagUrl = getFlag(item.original_language);
 
     if( flagUrl != ""){
 
@@ -61,12 +92,58 @@ function getResults(list){
     }
 
     else{
+
       dati.lingua = item.original_language;
       dati.flag = "";
     }
 
     printFilms(dati,voto);
   }
+}
+
+// chiamata api con in il type (tv, movie) in ingresso
+// per estrarre le serie tv , richiamo apiCall con type = tv se:
+// non è la prima chiamata (page = ""), se sono nell'ultima pagina dei movie  o se le pagine del movie è solo una
+
+function apiCall(search,page,type){
+
+ var outData = {
+
+   api_key: "be7a5b068bb701d40ef499c039960c53",
+   language: "it-IT",
+   query: search,
+   page: page,
+  }
+
+  var url = "https://api.themoviedb.org/3/search/" + type;
+
+  $.ajax({
+
+    url: url,
+    data: outData,
+    method: "GET",
+
+    success: function(inData, stato){
+
+      if(page == "" &&  inData.total_pages > 1 && type == "movie"){
+        genPages(inData.total_pages);
+      }
+
+      if (type == "movie" && ( page == inData.total_pages  || inData.total_pages == 1 || inData.total_results == 0 )){
+        apiCall(search,"","tv");
+      }
+
+      console.log(inData.results);
+      if(inData.total_results > 0){
+
+        getResults(inData.results,type);
+      }
+    },
+    error: function(request, stato ,error){
+
+      console.log("error");
+    }
+  })
 }
 
 function genPages(pageNum){
@@ -89,34 +166,6 @@ function genPages(pageNum){
   pages.children(".page-num").eq(0).addClass("current")
 }
 
-function apiCall(search,page){
-
- var outData = {
-
-   api_key: "be7a5b068bb701d40ef499c039960c53",
-   language: "it-IT",
-   query: search,
-   page: page,
-  }
-
-  $.ajax({
-
-    url: "https://api.themoviedb.org/3/search/movie",
-    data: outData,
-    method: "GET",
-    success: function(inData, stato){
-
-      if(page == "" ){genPages(inData.total_pages);}
-      getResults(inData.results);
-
-    },
-    error: function(request, stato ,error){
-
-      console.log("error");
-    }
-  })
-}
-
 function clickPage(){
 
   var pages = $(".pages");
@@ -126,7 +175,7 @@ function clickPage(){
     var pag = parseInt($(this).text(),10);
 
     $(".filmList").empty()
-    apiCall( $("#search-input").val(), pag);
+    apiCall( $("#search-input").val(), pag , "movie");
 
     pages.children().removeClass("current");
     $(this).addClass("current");
@@ -138,7 +187,7 @@ function getVal(elem){
 
   $(".filmList").empty();
   $(".pages").empty();
-  apiCall(elem.val(),"")
+  apiCall(elem.val(),"","movie")
 }
 
 function init(){
